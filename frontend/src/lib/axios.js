9b3e07cc3d873903b53c05b88ce9ab350df7b9df
cderry8 <derrickcyubahiroo@gmail.com>
2025-11-15 14:27:54 +0200

    all done

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chatapp-ypsb.onrender.com/api", // updated base URL
+  baseURL: "https://chatapp-ypsb.onrender.com/api", 
   withCredentials: true,
 });
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "https://chatapp-ypsb.onrender.com"; // updated socket URL
+const BASE_URL = "https://chatapp-ypsb.onrender.com"; 
 
 
 export const useAuthStore = create((set, get) => ({
