f368d3ea6a96084c705467e48fd927761b6f98ac
cderry8 <derrickcyubahiroo@gmail.com>
2025-11-15 13:48:17 +0200

    fixed import error

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
+import axios from "axios";
+
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
-      : import.meta.env.VITE_API_URL,
+      : "https://chatapp-ypsb.onrender.com/api",
   withCredentials: true,
 });
