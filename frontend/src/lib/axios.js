b64574f7b7716baf153072371053b78f52e556b7
cderry8 <derrickcyubahiroo@gmail.com>
2025-11-15 13:35:40 +0200

    configure base url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
-      : "https://chatapp-ypsb.onrender.com/api",
+      : import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
